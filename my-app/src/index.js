c507b3f263fdd0e670cdae7d44b03bae02cebaa8
dadyaDima <dmitriyyrievich@icloud.com>
2022-12-02 19:45:54 +0200

    project preparation

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Elem = (props) => {
+    return <h1>Hello world, my name is {props.title}  !!!</h1>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// old
+// reactDom.render(elem, document.getElementById('root'))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render( <Elem title='Dmitriy'/> )
+    // <React.StrictMode>
+    //     <App />
+    // </React.StrictMode>
